0e00de5e36dfa5f6863795ba232e7e9e0f0e899d
Sant Narayan Singh <santnarayansingh@gmail.com>
2022-08-31 16:27:02 +0530

    fixes bugs

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var progressBars = document.querySelectorAll(".skill-progress > div");
 
-
-
 function initialiseBar(bar) {
     bar.setAttribute("data-visited", false);
     bar.style.width = 0 + '%';
@@ -52,7 +50,7 @@ function checkScroll() {
 window.addEventListener("scroll", checkScroll);
 
 // This event fills the progress bars if they are displayed on the screen when the page is loaded.
-// //window.addEventListener("load", checkScroll);
+//window.addEventListener("load", checkScroll);
 
 
 
@@ -61,23 +59,31 @@ window.addEventListener("scroll", checkScroll);
 
 
 
-// var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+var interval;
 
-// for(var i = 0; i < navMenuAnchorTags.length; i++){
-//     navMenuAnchorTags[i].addEventListener('click', function(event) {
-//         event.preventDefault();
-//         var targetSectionID = this.textContent.trim().toLowercase();
-//         var targetSection = document.getElementById(targetSectionID);
-//         console.log(targetSection);
 
-//         var interval = setInterval(function() {
-            
-//             var targetSectionCoordinates = targetSection.getBoundingClientRect();
-//             if(targetSectionCoordinates.top <= 0) {
-//                 clearInterval(interval);
-//                 return;
-//             }
-//             window.scrollBy(0, 50);
-//         }, 20);
-//     });
-// }
+for (var i = 0; i < navMenuAnchorTags.length; i++) {
+    navMenuAnchorTags[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        var targetSectionID = this.textContent.trim().toLowerCase();
+        console.log(this.textContent);
+        var targetSection = document.getElementById(targetSectionID);
+        console.log(targetSection);
+        //    interval = setInterval(scrollVertically, 20, targetSection);
+
+        interval = setInterval(function () {
+            scrollVertically(targetSection);
+        }, 20);
+    });
+}
+
+
+function scrollVertically(targetSection) {
+    var targetSectionCoordinates = targetSection.getBoundingClientRect();
+    if (targetSectionCoordinates.top <= 0) {
+        clearInterval(interval);
+        return;
+    }
+    window.scrollBy(0, 50);
+}
